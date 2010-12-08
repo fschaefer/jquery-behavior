7b48fbf275a9c7102ee3e7bc9d3fec9b4fb543b7
fschaefer <florian.schaefer@gmail.com>
2010-12-08 10:54:43 +0100

    Handle $.behavior ([{ ... }, { ... }, ... ], [context]). Returned to Live Query instead of delegates, because of ajax events. Methods is alias for Helpers on Metabehaviors.

--- src/jquery.behavior.js
+++ src/jquery.behavior.js
@@ -6,12 +6,12 @@
  * Dual licensed under the MIT (MIT_LICENSE.txt)
  * and GPL Version 2 (GPL_LICENSE.txt) licenses.
  *
- * Version: 0.8
+ * Version: 0.9
  * Requires: jQuery 1.4.2+ and Live Query 1.1+ & Methods Plugin.
  * 
  */
 
-(function($, attr) {
+;(function($, attr) {
     
     // jQuery supports getData, setData and changeData,
     // support getAttr, setAttr, changeAttr too.
@@ -21,27 +21,27 @@
         
         if (!value) {
             // we are getting a value.
-            $.event.trigger ('getAttr', { 
-                attribute: name, 
-                from: current, 
-                to: value 
-            }, elem);    
+            $.event.trigger ('getAttr', {
+                attribute: name,
+                from: current,
+                to: value
+            }, elem);
         } else { // writing
             // we are setting a value.
-            $.event.trigger ('setAttr', { 
-                attribute: name, 
-                from: current, 
-                to: value 
+            $.event.trigger ('setAttr', {
+                attribute: name,
+                from: current,
+                to: value
             }, elem);
                     
             retval = attr (elem, name, value); // call original.
             
             // value or type changed.
             if (current !== value) {
-                $.event.trigger ('changeAttr', { 
-                    attribute: name, 
-                    from: current, 
-                    to: value 
+                $.event.trigger ('changeAttr', {
+                    attribute: name,
+                    from: current,
+                    to: value
                 }, elem);
             }
         }
@@ -50,23 +50,28 @@
     
 })(jQuery, jQuery.attr);
 
-(function($) {
+;(function($) {
 
     if (!$.fn.livequery) throw "jquery.behavior.js: jQuery Plugin: Live Query not loaded.";
     if (!($.fn.helpers = $.fn.methods)) throw "jquery.behavior.js: jQuery Plugin: Methods not loaded.";
 
     $.behavior = function (metabehaviors, context) {
         
-        context = context || document;
-        
-        var $context = $(context);
+        context = context || window.document;
         
         // Handle $.behavior (""), $.behavior (null), or $.behavior (undefined).
         if (!metabehaviors) {
             return this;
         }
         
-        // Handle $.behavior (metabehaviors, [context]).        
+        // Handle $.behavior ([{ ... }, { ... }, ... ], [context]).
+        if ($.isArray (metabehaviors)) {
+            return $.each (metabehaviors, function () {
+                $.behavior (this, context);
+            });
+        }
+        
+        // Handle $.behavior ({ ... }, [context]).
         return $.each (metabehaviors, function (selector, metabehavior) {
             
             metabehavior = $.extend (true, {
@@ -87,8 +92,12 @@
                 if (metabehavior.options.expire) {
                     $element.expire (metabehavior.transform, metabehavior.untransform);
                 }
-            
+                
                 $element.livequery (function () {
+                    
+                    if ($.isPlainObject (metabehavior.methods)) {
+                        $element.helpers (metabehavior.methods);
+                    }
                     
                     if ($.isPlainObject (metabehavior.helpers)) {
                         $element.helpers (metabehavior.helpers);
@@ -104,13 +113,13 @@
             
             // Bind all events.
             $.each (metabehavior.events, function (event, callback) {
-            
+                
                 if (metabehavior.options.expire) {
-                    $context.undelegate (selector, event);
+                    $element.expire (event);
                 }
                 
                 if ($.isFunction (callback)) {
-                    $context.delegate (selector, event, callback);
+                    $element.livequery (event, callback);
                 }
             });
             
