36832d7ecffa67579df74c92e61ae72aba8fa85e
Florian Schäfer <florian.schaefer@gmail.com>
2011-10-26 20:33:44 +0300

    Source code beautification.

--- src/jquery.behavior.js
+++ src/jquery.behavior.js
@@ -10,35 +10,34 @@
  * Requires: jQuery 1.4.2+ and Live Query 1.1+.
  * 
  */
+(function ($, attr) {
 
-(function($, attr) {
-    
     // jQuery supports getData, setData and changeData,
     // support getAttr, setAttr, changeAttr too.
-    $.attr = function (elem, name, value) { 
-        var current = attr (elem, name), // read current value.
-        retval = current;
-        
+    $.attr = function (elem, name, value) {
+        var current = attr(elem, name),
+            // read current value.
+            retval = current;
+
         if (!value) {
             // we are getting a value.
-            $.event.trigger ('getAttr', {
+            $.event.trigger('getAttr', {
                 attribute: name,
                 from: current,
                 to: value
             }, elem);
         } else { // writing
             // we are setting a value.
-            $.event.trigger ('setAttr', {
+            $.event.trigger('setAttr', {
                 attribute: name,
                 from: current,
                 to: value
             }, elem);
-            
-            retval = attr.apply (this, arguments); // call original.
-            
+
+            retval = attr.apply(this, arguments); // call original.
             // value or type changed.
             if (current !== value) {
-                $.event.trigger ('changeAttr', {
+                $.event.trigger('changeAttr', {
                     attribute: name,
                     from: current,
                     to: value
@@ -47,110 +46,110 @@
         }
         return retval; // return original 
     };
-    
+
 })(jQuery, jQuery.attr);
 
-(function($, undefined) {
-    
+(function ($, undefined) {
+
     if (!$.livequery) {
         throw "jquery.behavior.js: jQuery Plugin: Live Query not loaded.";
     }
-    
+
     $.behavior = function (metabehaviors, context) {
-        
+
         context = context || window.document;
-        
+
         // Handle $.behavior (""), $.behavior (null), or $.behavior (undefined).
         if (!metabehaviors) {
             return this;
         }
-        
+
         // Handle $.behavior ([{ ... }, { ... }, ... ], [context]).
-        if ($.isArray (metabehaviors)) {
-            return $.each (metabehaviors, function () {
-                $.behavior (this, context);
+        if ($.isArray(metabehaviors)) {
+            return $.each(metabehaviors, function () {
+                $.behavior(this, context);
             });
         }
-        
+
         // Handle $.behavior ({ ... }, [context]).
-        return $.each (metabehaviors, function (selector, metabehavior) {
-            
-            metabehavior = $.extend ({
+        return $.each(metabehaviors, function (selector, metabehavior) {
+
+            metabehavior = $.extend({
                 options: {
                     expire: false
                 },
                 transform: $.noop,
                 untransform: $.noop
             }, metabehavior);
-            
+
             // Cache element.
             var $elementInContext = $(selector, context),
                 $context = $(context);
-            
+
             // Transform DOM element.
-            $elementInContext.livequery (metabehavior.transform, metabehavior.untransform);
-            
+            $elementInContext.livequery(metabehavior.transform, metabehavior.untransform);
+
             // Bind all events.
             for (var event in metabehavior) {
-                
+
                 if (metabehavior.hasOwnProperty(event)) {
-                    
+
                     switch (event) {
-                        case 'transform':
-                        case 'untransform':
-                        case 'options':
-                            // Don't handle these here.
-                            continue;
-                        
-                        case 'blur': 
-                        case 'focus': 
-                        case 'focusin': 
-                        case 'focusout': 
-                        case 'load': 
-                        case 'resize': 
-                        case 'scroll': 
-                        case 'unload': 
-                        case 'click': 
-                        case 'dblclick': 
-                        case 'mousedown': 
-                        case 'mouseup': 
-                        case 'mousemove': 
-                        case 'mouseover': 
-                        case 'mouseout': 
-                        case 'mouseenter': 
-                        case 'mouseleave': 
-                        case 'change': 
-                        case 'select': 
-                        case 'submit': 
-                        case 'keydown': 
-                        case 'keypress': 
-                        case 'keyup': 
-                        case 'error':
-                            if (metabehavior.options.expire) {
-                                $context.undelegate (selector, event);
-                            }
-                            $context.delegate (selector, event, metabehavior[event]);
-                            break;
-                        
-                        default:
-                            if (metabehavior.options.expire) {
-                                $elementInContext.expire (event);
-                            }
-                            $elementInContext.livequery (event, metabehavior[event]);
-                            break;
+                    case 'transform':
+                    case 'untransform':
+                    case 'options':
+                        // Don't handle these here.
+                        continue;
+
+                    case 'blur':
+                    case 'focus':
+                    case 'focusin':
+                    case 'focusout':
+                    case 'load':
+                    case 'resize':
+                    case 'scroll':
+                    case 'unload':
+                    case 'click':
+                    case 'dblclick':
+                    case 'mousedown':
+                    case 'mouseup':
+                    case 'mousemove':
+                    case 'mouseover':
+                    case 'mouseout':
+                    case 'mouseenter':
+                    case 'mouseleave':
+                    case 'change':
+                    case 'select':
+                    case 'submit':
+                    case 'keydown':
+                    case 'keypress':
+                    case 'keyup':
+                    case 'error':
+                        if (metabehavior.options.expire) {
+                            $context.undelegate(selector, event);
+                        }
+                        $context.delegate(selector, event, metabehavior[event]);
+                        break;
+
+                    default:
+                        if (metabehavior.options.expire) {
+                            $elementInContext.expire(event);
+                        }
+                        $elementInContext.livequery(event, metabehavior[event]);
+                        break;
                     }
-                    
+
                 }
-                
+
             }
-            
+
         });
     };
-    
+
     $.fn.behavior = function (metabehaviors) {
-        return this.each (function () {
-            $.behavior (metabehaviors, this);
+        return this.each(function () {
+            $.behavior(metabehaviors, this);
         });
     };
-    
+
 })(jQuery);
